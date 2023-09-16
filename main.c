c75bd342fe2b0f3a342307a02d06f3b86ccd282a
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-16 23:16:59 +0300

    Update main.c

--- main.c
+++ main.c
@@ -29,11 +29,11 @@ int main(void)
 	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui); */
 	_printf("Character:[%c1%c1%c1%c]\n", 'H', 'b', 'g', 'c');
-	printf("Character:[%c]\n", 'H');
+	printf("Character:[%c1%c1%c1%c]\n", 'H', 'b', 'g', 'c');
 	_printf("String:[%s][%s][%s][%s]\n", "I am a string !", "I am a string !", "I am a string !", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s][%s][%s][%s]\n", "I am a string !", "I am a string !", "I am a string !", "I am a string !");
 	len = _printf("Percent:[%%][%%][%%][%%]\n");
-	len2 = printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%][%%][%%][%%]\n");
 	len2 = printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
 
