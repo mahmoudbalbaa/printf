7e85a3003e917cf059713898dac7f301951736fe
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-09-16 11:02:10 -0700

    function that produces output according to a format

--- printf.c
+++ printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...)
 	char c, *str;
 
 	va_start(arg, format);
-	for (i = 0; format[i]; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i]  == '%')
 		{
