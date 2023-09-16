8616280a8913c1437959feed491db66d71295977
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-17 02:26:46 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -27,8 +27,8 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			if (!format[i+1]||(format[i+1]==' '&&!format[i+2]))
-				return(-1);
+			if (!format[i + 1] || (format[i + 1] == ' ' && !format[i + 2]))
+				return (-1);
 			i++;
 			count += _define_specifier(format[i], arg);
 		}
