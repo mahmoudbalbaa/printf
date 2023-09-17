c405c80f41980773789bf9ec3e4ec79d1a617a66
Mahmoud Balbaa <138934815+mahmoudbalbaa@users.noreply.github.com>
2023-09-17 03:35:30 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -20,7 +20,9 @@ int _printf(const char *format, ...)
 	va_list arg;
 	int count = 0, i;
 
-	if (!format)
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	va_start(arg, format);
 	for (i = 0; format[i] != '\0'; i++)
