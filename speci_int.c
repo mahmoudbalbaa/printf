f170f247ff87255af898ba2ff88160dbfa9c3ec4
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-18 04:10:46 +0300

    Update speci_int.c

--- speci_int.c
+++ speci_int.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * speci_in - converts a string to a pointer
+ * speci_int -handling %i
  * @arg: an argument list
  * Return: NULL if error happens or str if not
  */
 
 char *speci_int(va_list arg)
 {
-	int in = va_arg(arg, int);
-	char buffer [sizeof(int)*8+1];
-	 itoa (in,buffer,10);
+	char *buffer;
+
+	buffer = malloc(sizeof(int) * 8 + 1);
+	 itoa(va_arg(arg, int), buffer, 10);
 	if (buffer == NULL)
 		return (NULL);
 
-	return (*buffer);
+	return (buffer);
 }
