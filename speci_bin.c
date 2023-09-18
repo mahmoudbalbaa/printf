51925a0e7603b755832881e58a62d9ec9953a46c
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-18 05:07:48 +0300

    Create speci_bin.c

---
+++ speci_bin.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * speci_int -handling %i
+ * @arg: an argument list
+ * Return: NULL if error happens or str if not
+ */
+  int int_to_binary(unsigned int k) {
+    if (k == 0 || k == 1) {
+        return k;
+    }
+    return (k % 2) + 10 * int_to_binary(k / 2);
+}
+
+char *speci_bin(va_list arg)
+{
+	char *buffer;
+  unsigned int x =va_arg(arg, int);
+  unsigned int bin =int_to_binary(x);
+
+	buffer = malloc(sizeof(int) * 8 + 1);
+	 itoa(bin, buffer, 10);
+	if (buffer == NULL)
+		return (NULL);
+
+	return (buffer);
+}
