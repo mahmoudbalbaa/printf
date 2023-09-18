730b6b3a982100994ac6b3713b333e3acb7a9613
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-09-18 15:46:00 -0700

    update betty

--- pf_bin.c
+++ pf_bin.c
@@ -2,7 +2,7 @@
 
 /**
  * int_to_binary - function that converts unsign int to binary.
- * @args: format string.
+ * @k: an integer.
  * Return: number of characters printed.
  */
 int int_to_binary(unsigned int k)
@@ -11,6 +11,7 @@ int int_to_binary(unsigned int k)
 		return (k);
 	return ((k % 2) + 10 * int_to_binary(k / 2));
 }
+
 /**
  * pf_bin - function that converts unsign int to binary.
  * @args: format string.
@@ -21,7 +22,7 @@ int int_to_binary(unsigned int k)
 int pf_bin(va_list args)
 {
 /*char *buffer;*/
-	int bin ,r;
+	int bin, r;
 int x = va_arg(args, int);
 	if (x < 0)
 		x *= -1;
