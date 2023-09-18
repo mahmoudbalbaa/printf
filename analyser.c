1b0126f36f788f5ecc72d94b8c94bab99e72ea76
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-09-18 06:13:17 -0700

    new trial

--- analyser.c
+++ analyser.c
@@ -12,7 +12,7 @@ int analyser(const char *format, va_list arg)
 	int i, count = 0;
 	char *buff = NULL;
 
-	if (format == NULL)
+	if ((format == NULL) || ((format[0] == '%') && (format[1] == '\0')))
 		return (-1);
 
 	for (i = 0; format != NULL && format[i]; i++)
--- itoa.c
+++ itoa.c
@@ -1,31 +1,36 @@
 #include "main.h"
+
+/**
+ * itoa - 
+ * /
+
 char *itoa(int value, char *str, int base) {
-    int i ,length;
-    char* ptr;
-    /* Handle negative numbers*/
-    if (value < 0 && base == 10) {
-        *str++ = '-';
-        value = -value;
-    }
+int i ,length;
+char* ptr;
+/* Handle negative numbers*/
+if (value < 0 && base == 10) {
+*str++ = '-';
+value = -value;
+}
 
-    /* Convert the number to a string in reverse order*/
-    ptr = str;
-    do {
-        int digit = value % base;
-        *ptr++ = digit < 10 ? '0' + digit : 'A' + digit - 10;
-        value /= base;
-    } while (value > 0);
+/* Convert the number to a string in reverse order*/
+ptr = str;
+do {
+int digit = value % base;
+*ptr++ = digit < 10 ? '0' + digit : 'A' + digit - 10;
+value /= base;
+} while (value > 0);
 
-    /* Add null terminator*/
-    *ptr = '\0';
+/* Add null terminator*/
+*ptr = '\0';
 
-    /* Reverse the string*/
-    length = ptr - str;
-    for (i = 0; i < length / 2; i++) {
-        char temp = str[i];
-        str[i] = str[length - i - 1];
-        str[length - i - 1] = temp;
-    }
+/* Reverse the string*/
+length = ptr - str;
+for (i = 0; i < length / 2; i++) {
+char temp = str[i];
+str[i] = str[length - i - 1];
+str[length - i - 1] = temp;
+}
 
-    return str;
+return str;
 }
