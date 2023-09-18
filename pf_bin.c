a13cdf7332dbe2881b403488f79189fd249809b0
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-19 01:11:52 +0300

    Update pf_bin.c

--- pf_bin.c
+++ pf_bin.c
@@ -1,3 +1,12 @@
+#include "main.h"
+
+/**
+ * pf_bin - function that converts unsign int to binary.
+ * @args: format string.
+ * Return: number of characters printed.
+ */
+
+
 int pf_bin(va_list args)
 {
 	int i, l;
