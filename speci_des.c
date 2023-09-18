d9dc7dd9d951efc464c86217ee988e3a1645003f
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-18 04:34:07 +0300

    Create speci_des.c

---
+++ speci_des.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * speci_int -handling %i
+ * @arg: an argument list
+ * Return: NULL if error happens or str if not
+ */
+
+char *speci_des(va_list arg)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(int) * 8 + 1);
+	 itoa(va_arg(arg, int), buffer, 10);
+	if (buffer == NULL)
+		return (NULL);
+
+	return (buffer);
+}
