ea574ecf89a15e81d8e7d4230e1ca13f7b1446d5
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-19 03:09:50 +0300

    Update pf_oct.c

--- pf_oct.c
+++ pf_oct.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * to_octal - function that prints string.
+ * @num: input int num.
+ * Return: number of characters printed.
+ */
+int to_octal(unsigned int num)
+{
+
+    int octal[100];
+    int i = 0;
+
+    while (num != 0) {
+        octal[i] = num % 8;
+        num /= 8;
+        i++;
+    }
+
+    for (int j = i - 1; j >= 0; j--) {
+        print_number(octal[j]);
+    }
+	return (i);
+}
+/**
  * pf_oct - function that prints string.
  * @args: format string.
  * Return: number of characters printed.
@@ -10,7 +32,7 @@ int pf_oct(va_list args)
 {
 	int i, l;
 
-	i = va_arg(args, int);
-	l = print_number(i);
+	i = to_octal(va_arg(args, unsigned int));
+
 	return (l);
 }
