2515017b85c6cb25f09d712ad081e2d51ae94aa4
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-20 00:34:41 +0300

    Update hexa_code.c

--- hexa_code.c
+++ hexa_code.c
@@ -20,6 +20,19 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
-
 	return (3);
+}
+/**
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated.
+ *
+ * Return: 1 if c is printable, 0 otherwise
+ */
+
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
 }
