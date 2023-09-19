9b52d202e8d61a5a39ffc78b560540935aa0c56a
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-19 03:18:57 +0300

    Update pf_bin.c

--- pf_bin.c
+++ pf_bin.c
@@ -1,30 +1,35 @@
 #include "main.h"
 
 /**
- * int_to_binary - function that converts unsign int to binary.
+ * pf_bin - function that converts unsign int to binary.
  * @k: an integer.
  * Return: number of characters printed.
  */
-int int_to_binary(unsigned int k)
-{
-	if (k == 0 || k == 1)
-		return (k);
-	return ((k % 2) + 10 * int_to_binary(k / 2));
-}
-
-/**
- * pf_bin - function that converts unsign int to binary.
- * @args: format string.
- * Return: number of characters printed.
- */
-
-
 int pf_bin(va_list args)
 {
-/*char *buffer;*/
-	int bin, r;
-int x = va_arg(args, unsigned int);
-bin = int_to_binary(x);
-r = print_number(bin);
-return (r);
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+
+	n = va_arg(args, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
 }
