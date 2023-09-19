f84a3d2c5add9f15ece326eb0278ad8da8d46af1
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-19 06:41:53 +0300

    Update pf_Non.c

--- pf_Non.c
+++ pf_Non.c
@@ -17,7 +17,7 @@ int pf_Non(va_list args)
 
 	while (str[i] != '\0')
 	{
-	if (str[i] >= 32 && str[i] < 127)
+	if (str[i] >= 32 && (int)str[i] < 127)
 			buffer[i + offset] = str[i];
 		else
 			offset += (int)str[i];
