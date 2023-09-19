11a7f121469794baa09c482f88256575ddbd0fa1
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-09-19 12:27:04 -0700

    update betty

--- pf_uhex.c
+++ pf_uhex.c
@@ -1,51 +1,59 @@
 #include "main.h"
 
 /**
- * to_octal - function that prints string.
+ * to_uhex - function that prints string.
  * @num: input int num.
  * Return: number of characters printed.
  */
-char* to_uhex(unsigned int num) {
-    FILE *fp;
-    static char hex[100];
-    int i = 0;
-    if (num == 0) {
-        return "0";
-    }
 
+char *to_uhex(unsigned int num)
+{
+	FILE *fp;
+	static char hex[100];
+	int i = 0;
 
-    while (num != 0) {
-        int remainder = num % 16;
-        if (remainder < 10) {
-            hex[i] = remainder + '0';
-        } else {
-            hex[i] = remainder + 'A' - 10;
-        }
-        num /= 16;
-        i++;
-    }
+	if (num == 0)
+	{
+		return (0);
+	}
 
-    fp = fopen("file.txt", "w");
-    fwrite(hex, sizeof(char), i, fp);
-    fclose(fp);
+	while (num != 0)
+	{
+		int remainder = num % 16;
 
-    hex[i] = '\0'; /* Null-terminate the string*/
-    return hex;
+		if (remainder < 10)
+		{
+			hex[i] = remainder + '0';
+		}
+		else
+		{
+			hex[i] = remainder + 'A' - 10;
+		}
+		num /= 16;
+		i++;
+	}
+	fp = fopen("file.txt", "w");
+	fwrite(hex, sizeof(char), i, fp);
+	fclose(fp);
+	hex[i] = '\0'; /* Null-terminate the string*/
+	return (hex);
 }
 
 /**
- * pf_oct - function that prints string.
+ * pf_uhex - function that prints string.
  * @args: format string.
  * Return: number of characters printed.
  */
 
 int pf_uhex(va_list args)
 {
-	char* x;
-	int i ,l;
+	char *x;
+	int i, l;
+
 	x = to_uhex(va_arg(args, unsigned int));
 	l = _strlen(x);
-	for (i = l-1; i >= 0; i--)
+	for (i = l - 1; i >= 0; i--)
 		_putchar(x[i]);
+
 	return (l);
 }
