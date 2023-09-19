a5c63cfe4e518e1d3a046031261dd5874beee525
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-09-19 07:19:45 -0700

    update betty

--- _printf.c
+++ _printf.c
@@ -11,16 +11,11 @@ int _printf(const char *format, ...)
 	va_list args;
 	int i = 0, j, l = 0;
 	convert specs[] = {
-		{"%s", pf_string},
-		{"%c", pf_char},
-		{"%%", pf_perc},
-		{"%d", pf_dec},
-		{"%i", pf_int},
-		{"%b", pf_bin},
-		{"%u", pf_uns},
-		{"%o", pf_oct},
-		{"%x", pf_lhex},
-		{"%X", pf_uhex},
+		{"%s", pf_string}, {"%c", pf_char},
+		{"%%", pf_perc}, {"%d", pf_dec},
+		{"%i", pf_int}, {"%b", pf_bin},
+		{"%u", pf_uns}, {"%o", pf_oct},
+		{"%x", pf_lhex}, {"%X", pf_uhex},
 		{"%S", pf_Non}
 	};
 
@@ -28,7 +23,7 @@ int _printf(const char *format, ...)
 	if ((format == NULL) || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-Here:
+again:
 	while (format[i] != '\0')
 	{
 		j = 0;
@@ -39,7 +34,7 @@ Here:
 			{
 				l += specs[j].f(args);
 				i += 2;
-				goto Here;
+				goto again;
 			}
 			j++;
 		}
