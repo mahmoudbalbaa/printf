bb1fed26ab6dcfe64e302101739c9e99546fc24b
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-09-19 09:34:46 -0700

    create pf_string.c

--- 1-function1.c
+++ 1-function1.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * pf_string - function that prints string.
- * @args: format string.
- * Return: number of characters printed.
- */
-
-int pf_string(va_list args)
-{
-	char *str;
-	int i, l;
-
-	str = va_arg(args, char *);
-	if (str == NULL)
-		str = "(null)";
-	l = _strlen(str);
-	for (i = 0; i < l; i++)
-		_putchar(str[i]);
-	return (l);
-}
-
-/**
  * pf_char - function that prints pecentage  sign.
  * @args: format string.
  * Return: number of characters printed.
