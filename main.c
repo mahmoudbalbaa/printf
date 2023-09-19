3286f3d15598c87ea98dd014b2d00678ffed9402
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-19 04:45:45 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,11 +8,16 @@
 int main(void)
 {
     int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
 
     len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
-    _printf("Length:[%b, %b]\n", 98, -98);
-  /*  printf("Length:[%d, %i]\n", len2, len2);
+    printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
@@ -31,7 +36,7 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-_printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-   */ return (0);
+   /* _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");*/
+    return (0);
 }
