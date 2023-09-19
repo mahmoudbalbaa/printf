c74368af0fa04acb12aabf30b5d6f0dadb5734b9
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-09-19 13:49:32 -0700

    a new update in pf_lhex.c

--- pf_lhex.c
+++ pf_lhex.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * to_lhex - Converts an unsigned int to a lowercase hexadecimal string.
- * @num: The unsigned int to be converted.
- * Return: The resulting string.
+ * to_lhex - function that prints string.
+ * @num: input int num.
+ * Return: number of characters printed.
  */
-
 char *to_lhex(unsigned int num)
 {
+	FILE *fp;
 	static char hex[100];
 	int i = 0;
 
 	if (num == 0)
 	{
-		hex[i++] = '0';
-		hex[i] = '\0';
-		return (hex);
+		return (0);
 	}
+
 
 	while (num != 0)
 	{
@@ -33,36 +32,27 @@ char *to_lhex(unsigned int num)
 		num /= 16;
 		i++;
 	}
-
+	fp = fopen("file.txt", "w");
+	fwrite(hex, sizeof(char), i, fp);
+	fclose(fp);
 	hex[i] = '\0'; /* Null-terminate the string*/
-
-	/* Reverse the string */
-	for (int start = 0, end = i - 1; start < end; start++, end--)
-	{
-		char temp = hex[start];
-
-		hex[start] = hex[end];
-		hex[end] = temp;
-	}
-
 	return (hex);
 }
 
 /**
- * pf_lhex - Prints an unsigned int as a lowercase hexadecimal string.
- * @args: A va_list pointing to the unsigned int to be printed.
- * Return: The number of characters printed.
+ * pf_lhex - function that prints string.
+ * @args: format string.
+ * Return: number of characters printed.
  */
 
 int pf_lhex(va_list args)
 {
 	char *x;
-	int l;
+	int i, l;
 
 	x = to_lhex(va_arg(args, unsigned int));
 	l = _strlen(x);
-
-	for (int i = 0; i < l; i++)
+	for (i = l - 1; i >= 0; i--)
 		_putchar(x[i]);
 
 	return (l);
