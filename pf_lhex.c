61698d6e780689a8821d7980bdab160917291ffd
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-19 05:32:15 +0300

    Update pf_lhex.c

--- pf_lhex.c
+++ pf_lhex.c
@@ -45,7 +45,7 @@ int pf_lhex(va_list args)
 	int i ,l;
 	x = to_lhex(va_arg(args, unsigned int));
 	l = _strlen(x);
-	for (i = l-1; i > l; i--)
+	for (i = l-1; i >= 0; i--)
 		_putchar(x[i]);
 	return (l);
 }
