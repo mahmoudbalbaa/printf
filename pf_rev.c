a020bb8a13f36b4a0fb5b52e08e985eb3b91de8c
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-09-19 16:53:27 -0700

    update pf_rev.c

--- pf_rev.c
+++ pf_rev.c
@@ -1,35 +1,27 @@
 #include "main.h"
 
 /**
- * pf_rev - Prints reverse string.
- * @args: Lista of arguments
- * Return: Numbers of chars printed
+ * pf_rev - function that prints a reversed string.
+ * @args: format string.
+ * Return: number of characters printed.
  */
 
 int pf_rev(va_list args)
 {
-	char *str;
-	int i, count = 0;
-
-str = va_arg(args, char *);
+	char *str = va_arg(args, char *);
+	int len = 0, count = 0;
 
 	if (str == NULL)
+		return (-1);
+
+	for (; str[len] != '\0'; len++)
+		;
+
+	for (int i = len - 1; i >= 0; i--)
 	{
-		str = "%r";
-		_putchar(str[0]);
-		_putchar(str[1]);
-		return (2);
+		_putchar(str[i]);
+		count++;
 	}
-	for (i = 0; str[i]; i++)
-		printf("%c", str[i]);
 
-	for (i = i - 1; i >= 0; i--)
-	{
-		/*char z = str[i];*/
-
-		char z = str[i];
-
-		_putchar(z);
-}
-return (count);
+	return (count);
 }
